8fc040e0e58e0ad3f0a0538571b62e2c0b49894e
Dylan McAulay <dmcaulay97@gmail.com>
2023-01-02 16:50:21 -0500

    added initial files and reated mouse responsive border

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [angle, setAngle] = useState("180deg")
+
+  useEffect(() => {
+    const mouseMoveAnimation = (event) => {
+      const absolute_x = event.x;
+      const absolute_y = window.innerHeight - event.y;
+      const screen_x = window.innerWidth;
+      const screen_y = window.innerHeight;
+      const x = absolute_x - (screen_x / 2)
+      const y = absolute_y - (screen_y / 2)
+      const theta_prime = Math.atan(x / y) * (180 / Math.PI)
+      if (x > 0 && y > 0) {
+        setAngle(`${theta_prime}deg`)
+      } else if (x > 0 && y < 0) {
+        setAngle(`${180 + theta_prime}deg`)
+      } else if (x < 0 && y < 0) {
+        setAngle(`${180 + theta_prime}deg`)
+      } else if (x < 0 && y > 0) {
+        setAngle(`${theta_prime}deg`)
+      }
+
+    }
+
+    const screen = document.getElementsByClassName("App")[0]
+    screen.addEventListener("mousemove", mouseMoveAnimation)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='border-wrap' style={{ background: `linear-gradient(${angle}, red, #1d1d20` }}>
+        <div className="container">
+          <div className='header'>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
